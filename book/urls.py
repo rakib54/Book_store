36f11d8e248ec881ddf29e4f75dadbb02eb1cf94
rakib54 <rakibur5454@gmail.com>
2023-06-05 18:43:28 +0600

    added Edit and delete books functionality

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from book.views import home, store_book, show_books
+from book.views import home, store_book, show_books, edit_book, delete_books
 
 urlpatterns = [
     path('', home),
     path('store_new_book/', store_book, name="store_book"),
-    path('show_books/', show_books, name="show_books")
+    path('show_books/', show_books, name="show_books"),
+    path('edit_books/<int:id>', edit_book, name="edit_books"),
+    path('delete_books/<int:id>', delete_books, name="delete_books"),
 ]
--- book/views.py
+++ book/views.py
@@ -26,3 +26,19 @@ def show_books(request):
     books = BookstoreModel.objects.all()  # take all data from database
     print('ALL books', books)
     return render(request, 'show_book.html', {'books': books})
+
+
+def edit_book(request, id):
+    book = BookstoreModel.objects.get(pk=id)
+    form = BookStoreForm(instance=book)
+    if (request.method == 'POST'):
+        form = BookStoreForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('show_books')
+    return render(request, 'store_book.html', {'form': form})
+
+
+def delete_books(request, id):
+    book = BookstoreModel.objects.get(pk=id).delete()
+    return redirect('show_books')
